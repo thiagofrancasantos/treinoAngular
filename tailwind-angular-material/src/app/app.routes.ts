91ef13c77bc9e3ccaaea762dd77f9a273492b89c
Thiago Franca <francasantos.t@gmail.com>
2025-11-02 19:15:34 +0000

    tailwind home done

--- tailwind-angular-material/src/app/app.routes.ts
+++ tailwind-angular-material/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'sidebar',
+    component: SidebarComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+];
